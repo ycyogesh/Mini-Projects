d2ba8d99194d160092d3ab88dc883839ec7dc02e
ycyogesh <ycyogesh183@gmail.com>
2022-08-18 14:36:52 +0530

    split function

--- my_calc/split.js
+++ my_calc/split.js
@@ -1,7 +1,6 @@
 str = "3+5/66*6-8"
 let op = str.split(/[0-9]/).filter(e => e!='')
 let val = str.split(/[+*-/]/)
-var sum = 0
 // str = str.split("")
 // String.split()
 console.log(op);
@@ -23,10 +22,10 @@ for(let i = 0;i<str.length;i++){
    else if(op[i]=="%"){
     val[i+1]= +val[i] % +val[i+1]
    }else{
-    console.log("Check Operator");
    }
 }
-console.log(sum);
+let a = val.length-1
+console.log(val[a]);
 
 
 // console.log(str);
