5b039c3cb1cf9fdb5eadae6e2c24ca22a420bc9a
ycyogesh <ycyogesh183@gmail.com>
2022-08-18 15:45:41 +0530

    server calculator

--- my_calc/calc_server.js
+++ my_calc/calc_server.js
@@ -5,10 +5,10 @@ var app = express();
 
 app.get("/",(req,res)=>{
     var calci = req.query.calc;
-    console.log(calci);
-    calci = calci.split("");
-    // myFunction(calci);
-    res.send(calci)
+    console.log(req.query.calc);
+    var answer = myFunction(calci);
+    console.log(answer);
+    res.send(""+answer)
 
 })
 
@@ -19,8 +19,32 @@ app.listen(3000,()=>{
 
 
 
-// function myFunction(a){
-//     for(let i = 0;i<a.length;i++){
+function myFunction(a){
+let op = a.split(/[0-9]/).filter(e => e!='')
+let val = a.split(/[+*-/"]/)
 
-//     }
-// }
+for(let i = 0;i<a.length;i++){
+   if(op[i]=="+"){
+    val[i+1] = +val[i] + +val[i+1]
+   }
+   else if(op[i]=="*"){
+    val[i+1] = +val[i] * +val[i+1]
+   }
+   else if(op[i]=="-"){
+    val[i+1]= +val[i] - +val[i+1]
+   }
+   else if(op[i]=="/"){
+    val[i+1] = +val[i] / +val[i+1]
+   }
+   else if(op[i]=="%"){
+    val[i+1]= +val[i] % +val[i+1]
+   }else{
+   }
+}
+// console.log(val);
+let c = +(val.length-2)
+// console.log(c);
+let value = +val[c]
+// console.log(value);
+return value
+}
