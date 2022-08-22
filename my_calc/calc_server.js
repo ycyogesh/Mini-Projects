9f11ca50f0c2f3c8e28483e484182e68b86a2998
ycyogesh <ycyogesh183@gmail.com>
2022-08-22 18:46:59 +0530

    added code

--- my_calc/calc_server.js
+++ my_calc/calc_server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 
 const cors = require('cors')
 
@@ -8,17 +8,17 @@ const cors = require('cors')
 //   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
 // }
 var app = express();
-app.use(cors);
+app.use(cors());
 
 // app.use(cors({
 //     origin: '*'
 // }));
 
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5501');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-    next();
-})
+// app.use(function(req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5501');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+//     next();
+// })
 
 // app.use(function(req, res, next) {
 //     console.log("dsdfdsfdsfds",req.query.calc)
@@ -34,12 +34,13 @@ app.get("/",(req,res)=>{
     // console.log(req.query.calc);
     var answer = myFunction(calci);
     console.log(answer);
-    res.send(""+answer)
+    // res.send(""+answer)
+    res.json({"ans": answer})
     // res.send()
 
 })
 
-app.listen(3000,()=>{
+app.listen(3001,()=>{
     console.log("App Running");
 })
 
